59bcaad99e7caca9aef94753e31acd22a398ea3d
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-01-22 15:07:56 -0600

    production URL changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,12 @@ export const environment = {
   production: true
 };
 
-export const URL_SERVICES = 'https://rest-api-ecommerce-production.up.railway.app/api/';
+// export const URL_SERVICES = 'https://rest-api-ecommerce-production.up.railway.app/api/';
 
-export const SOCKET = 'https://rest-api-ecommerce-production.up.railway.app'
+// export const SOCKET = 'https://rest-api-ecommerce-production.up.railway.app'
+
+export const URL_SERVICES = 'https://ecommerce-shoper.onrender.com/api/';
+
+export const SOCKET = 'https://ecommerce-shoper.onrender.com'
 
 export const URL_TIENDA = 'https://shoper-store.netlify.app/';
